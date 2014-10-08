c72e75a8b11cadd14c566f98c7256c60da8b7e43
jflute <dbflute@gmail.com>
2014-10-08 13:15:19 +0900

    #5: for example's rename

--- dbflute-engine/deploy-script/engine-dist-deploy.sh
+++ dbflute-engine/deploy-script/engine-dist-deploy.sh
@@ -1,19 +1,26 @@
 cd ..
 ant -f build.xml dist
-#ant -f build.xml reflect-to-test-active-dockside
+ant -f build.xml reflect-to-test-option-compatible10x
+ant -f build.xml reflect-to-test-active-dockside
 ant -f build.xml reflect-to-test-active-hanger
 
-cd ../../dbflute-test-active-dockside/dbflute_exampledb
+cd ..
+export answer=y
+
+cd ../dbflute-test-option-compatible10x/dbflute_maihamadb
 rm ./log/*.log
-. jdbc.sh
-. doc.sh
-. generate.sh
-. sql2entity.sh
+. manage.sh renewal
 cd ..
 mvn -e compile
 
-cd ../dbflute-test-active-hanger/dbflute_exampledb
+cd ../dbflute-test-active-dockside/dbflute_maihamadb
 rm ./log/*.log
-. manage.sh regenerate
+. manage.sh renewal
+cd ..
+mvn -e compile
+
+cd ../dbflute-test-active-hanger/dbflute_maihamadb
+rm ./log/*.log
+. manage.sh renewal
 cd ..
 mvn -e compile
