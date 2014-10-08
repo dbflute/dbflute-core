c922c664c731e1c202e786a651f1149894354c9c
jflute <dbflute@gmail.com>
2014-10-08 13:14:55 +0900

    #5: remove MY_PROJECT_NAME (already no needed)

--- dbflute-engine/embedded/etc/client-template/_project.bat
+++ dbflute-engine/embedded/etc/client-template/_project.bat
@@ -4,8 +4,6 @@ set ANT_OPTS=-Xmx512m
 
 set DBFLUTE_HOME=..\mydbflute\dbflute-@dbflute.version@
 
-set MY_PROJECT_NAME=dfclient
-
 set MY_PROPERTIES_PATH=build.properties
 
 if "%pause_at_end%"=="" set pause_at_end=y
