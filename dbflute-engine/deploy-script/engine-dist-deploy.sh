c65285bfc0aaa5d02bc2b5717f67835576003d4b
jflute <dbflute@gmail.com>
2014-10-13 02:20:43 +0900

    #5: add MySQL test

--- dbflute-engine/deploy-script/engine-dist-deploy.sh
+++ dbflute-engine/deploy-script/engine-dist-deploy.sh
@@ -1,17 +1,13 @@
 cd ..
 ant -f build.xml dist
-ant -f build.xml reflect-to-test-option-compatible10x
 ant -f build.xml reflect-to-test-active-dockside
 ant -f build.xml reflect-to-test-active-hanger
+ant -f build.xml reflect-to-test-dbms-mysql
+ant -f build.xml reflect-to-test-dbms-postgresql
+ant -f build.xml reflect-to-test-option-compatible10x
 
 cd ..
 export answer=y
-
-cd ../dbflute-test-option-compatible10x/dbflute_maihamadb
-rm ./log/*.log
-. manage.sh renewal
-cd ..
-mvn -e compile
 
 cd ../dbflute-test-active-dockside/dbflute_maihamadb
 rm ./log/*.log
@@ -20,6 +16,26 @@ cd ..
 mvn -e compile
 
 cd ../dbflute-test-active-hanger/dbflute_maihamadb
+rm ./log/*.log
+. manage.sh renewal
+cd ..
+mvn -e compile
+
+cd ../dbflute-test-dbms-mysql/dbflute_maihamadb
+rm ./log/*.log
+# not replace-schema because of big data
+. manage.sh regenerate
+cd ..
+mvn -e compile
+
+cd ../dbflute-test-dbms-postgresql/dbflute_maihamadb
+rm ./log/*.log
+# not replace-schema because of big data
+. manage.sh regenerate
+cd ..
+mvn -e compile
+
+cd ../dbflute-test-option-compatible10x/dbflute_maihamadb
 rm ./log/*.log
 . manage.sh renewal
 cd ..
