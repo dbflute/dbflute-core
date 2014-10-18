30724bae5b82d5398c4bfbd5df9891f4f00f7a58
jflute <dbflute@gmail.com>
2014-10-18 19:43:56 +0900

    #5: for quick generation

---
+++ dbflute-engine/deploy-script/quick-engine-dist-deploy.sh
@@ -0,0 +1,12 @@
+cd ..
+ant -f build.xml dist
+ant -f build.xml reflect-to-test-active-dockside
+
+cd ..
+export answer=y
+
+cd ../dbflute-test-active-dockside/dbflute_maihamadb
+rm ./log/*.log
+. manage.sh regenerate
+cd ..
+mvn -e compile
