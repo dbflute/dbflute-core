7b0a031bee80a2ecd2805405d473de12b383e4a2
jflute <dbflute@gmail.com>
2014-11-02 17:41:53 +0900

    #10: suppress warning for list property e.g. Oracle's array type

--- dbflute-engine/src/main/java/org/apache/torque/engine/database/model/Table.java
+++ dbflute-engine/src/main/java/org/apache/torque/engine/database/model/Table.java
@@ -802,6 +802,15 @@ public class Table {
         return false;
     }
 
+    public boolean hasListColumn() { // e.g. Oracle's array type of structure
+        for (Column column : _columnList) {
+            if (column.isJavaNativeUtilList()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     // ===================================================================================
     //                                                                         Primary Key
     //                                                                         ===========
