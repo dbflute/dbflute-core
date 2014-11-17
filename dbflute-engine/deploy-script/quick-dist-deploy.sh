f217c15dd914974d5859026f85efc4c5dc2d390a
jflute <dbflute@gmail.com>
2014-11-18 00:05:00 +0900

    #10: add hanger stage

--- dbflute-engine/deploy-script/quick-dist-deploy.sh
+++ dbflute-engine/deploy-script/quick-dist-deploy.sh
@@ -1,11 +1,17 @@
 cd ..
 ant -f build.xml dist
 ant -f build.xml reflect-to-test-active-dockside
+ant -f build.xml reflect-to-test-active-hanger
 
 cd ..
 export answer=y
 
 cd ../dbflute-test-active-dockside/dbflute_maihamadb
+rm ./log/*.log
+. manage.sh regenerate
+cd ..
+
+cd ../dbflute-test-active-hanger/dbflute_maihamadb
 rm ./log/*.log
 . manage.sh regenerate
 cd ..
