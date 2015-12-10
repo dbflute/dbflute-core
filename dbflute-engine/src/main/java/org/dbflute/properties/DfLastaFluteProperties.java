c3bc74276e0f84c706fc5245f05357d2f4967e74
jflute <dbflute@gmail.com>
2015-12-10 22:36:09 +0900

    except DBFlute central project from application outsideSql for lastaflute single project

--- dbflute-engine/src/main/java/org/dbflute/properties/DfLastaFluteProperties.java
+++ dbflute-engine/src/main/java/org/dbflute/properties/DfLastaFluteProperties.java
@@ -148,7 +148,7 @@ public final class DfLastaFluteProperties extends DfAbstractHelperProperties {
         final Map<String, Object> lastafluteMap = getLastafluteMap();
         @SuppressWarnings("unchecked")
         final Map<String, Map<String, Object>> appMap = (Map<String, Map<String, Object>>) lastafluteMap.get("appMap");
-        if (appMap == null) {
+        if (appMap == null) { // just in case
             return;
         }
         logger.info("...Loading application outsideSql settings from lastafluteMap.");
@@ -159,7 +159,7 @@ public final class DfLastaFluteProperties extends DfAbstractHelperProperties {
             final Map<String, Object> map = entry.getValue();
             if (map != null) {
                 final String path = (String) map.get("path");
-                if (path != null) {
+                if (path != null && !path.equals("..")) { // e.g. except DBFlute central project e.g. harbor
                     final Map<String, String> elementMap = newLinkedHashMap();
                     elementMap.put("sqlDirectory", resourceDirectory);
                     elementMap.put("sql2EntityOutputDirectory", programDirectory);
