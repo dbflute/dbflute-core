ec02470b870b50beeb0e5fcba465f44c98e57089
jflute <dbflute@gmail.com>
2016-08-28 16:35:39 +0900

    DBFlute Engine: add MacOS JAVA_HOME setting to _project.sh

--- dbflute-engine/embedded/etc/client-template/_project.sh
+++ dbflute-engine/embedded/etc/client-template/_project.sh
@@ -5,3 +5,7 @@ export ANT_OPTS=-Xmx512m
 export DBFLUTE_HOME=../mydbflute/dbflute-@dbflute.version@
 
 export MY_PROPERTIES_PATH=build.properties
+
+if [ `uname` = "Darwin" ]; then
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+fi
