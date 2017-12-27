d5d31de11dfcadcf7200342b311e5906dfe5b062
江熊夕人 <yuto.eguma@bizreach.co.jp>
2017-12-28 00:48:53 +0900

    [test] add test deco map file and merge test

--- dbflute-runtime/src/test/java/org/dbflute/infra/doc/decomment/DfDecoMapFileTest.java
+++ dbflute-runtime/src/test/java/org/dbflute/infra/doc/decomment/DfDecoMapFileTest.java
@@ -22,7 +22,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.dbflute.infra.doc.decomment.parts.DfDecoMapColumnPart;
+import org.dbflute.infra.doc.decomment.parts.DfDecoMapPropertyPart;
+import org.dbflute.infra.doc.decomment.parts.DfDecoMapTablePart;
 import org.dbflute.optional.OptionalThing;
 import org.dbflute.unit.RuntimeTestCase;
 
@@ -35,52 +39,170 @@ public class DfDecoMapFileTest extends RuntimeTestCase {
     // ===================================================================================
     //                                                                          Definition
     //                                                                          ==========
-    private static final Long LATEST_COMMENT_VERSION = 3L;
+    private static final String TEST_RESOURCES_PATH = "/src/test/resources";
 
     // ===================================================================================
     //                                                                               Merge
     //                                                                               =====
-    public void test_mergeAllColumnComment() throws Exception {
+    public void test_merge_OnlyColumnComment() throws Exception {
         // ## Arrange ##
-        DfDecoMapFile decoMapFile = new DfDecoMapFile();
-        OptionalThing<DfDecoMapPickup> optPickup = OptionalThing.empty(); // not exists pickup
-        LocalDateTime now = currentLocalDateTime();
-        DfDecoMapPiece piece1 = preparePiece("MEMBER", "MEMBER_NAME", "hakiba", LATEST_COMMENT_VERSION, now);
-        DfDecoMapPiece piece2 = preparePiece("MEMBER", "MEMBER_STATUS", "cabos", LATEST_COMMENT_VERSION, now);
-        DfDecoMapPiece piece3 = preparePiece("PURCHASE", "PURCHASE_PRODUCT", "cabos", LATEST_COMMENT_VERSION, now);
-        List<DfDecoMapPiece> pieceList = Arrays.asList(piece1, piece2, piece3);
+        final DfDecoMapFile decoMapFile = new DfDecoMapFile();
+        final OptionalThing<DfDecoMapPickup> optPickup = OptionalThing.empty(); // not exists pickup
+        final DfDecoMapPiece piece1 = preparePiece(DfDecoMapPieceTargetType.Column,"MEMBER", "MEMBER_NAME", "hakiba", "SAMPLEPI", Collections.emptyList());
+        final DfDecoMapPiece piece2 = preparePiece(DfDecoMapPieceTargetType.Column,"MEMBER", "MEMBER_STATUS", "cabos", "ECECODED", Collections.emptyList());
+        final DfDecoMapPiece piece3 = preparePiece(DfDecoMapPieceTargetType.Column,"PURCHASE", "PURCHASE_PRODUCT", "cabos", "AYOOOOO", Collections.emptyList());
+        final List<DfDecoMapPiece> pieceList = Arrays.asList(piece1, piece2, piece3);
 
         // ## Act ##
-        DfDecoMapPickup result = decoMapFile.merge(optPickup, pieceList);
+        final DfDecoMapPickup result = decoMapFile.merge(optPickup, pieceList);
 
         // ## Assert ##
         assertNotNull(result);
         log(result);
-        // assert all table and column
-        long columnCount = result.getTableList()
-                .stream()
-                .map(tablePart -> tablePart.getColumnList())
-                .flatMap(columnParts -> columnParts.stream())
-                .count();
-        assertEquals(3, columnCount);
+
+        {
+            DfDecoMapTablePart member = extractPickupTableAsOne(result, "MEMBER");
+            assertEquals(0, member.getPropertyList().size());
+            assertEquals(2, member.getColumnList().size());
+            {
+                DfDecoMapColumnPart memberName = extractPickupColumnAsOne(member, "MEMBER_NAME");
+                assertEquals(1, memberName.getPropertyList().size());
+                assertEquals("hakiba", memberName.getPropertyList().get(0).getPieceOwner());
+            }
+            {
+                DfDecoMapColumnPart memberStatus = extractPickupColumnAsOne(member, "MEMBER_STATUS");
+                assertEquals(1, memberStatus.getPropertyList().size());
+                assertEquals("cabos", memberStatus.getPropertyList().get(0).getPieceOwner());
+            }
+        }
+        {
+            DfDecoMapTablePart member = extractPickupTableAsOne(result, "PURCHASE");
+            assertEquals(0, member.getPropertyList().size());
+            assertEquals(1, member.getColumnList().size());
+            {
+                DfDecoMapColumnPart purchaseProduct = extractPickupColumnAsOne(member, "PURCHASE_PRODUCT");
+                assertEquals(1, purchaseProduct.getPropertyList().size());
+                assertEquals("cabos", purchaseProduct.getPropertyList().get(0).getPieceOwner());
+            }
+        }
     }
 
-    private DfDecoMapPiece preparePiece(String tableName, String columnName, String author, long commentVersion,
-            LocalDateTime decommentDateTime) {
+    public void test_merge_OverwritePickupByPiece() {
+        // ## Arrange ##
+        final DfDecoMapFile decoMapFile = new DfDecoMapFile();
+        final String tableName = "MEMBER_LOGIN";
+        final String pieceCode = "NEWPIECE";
+        final String author = "borderman";
+        final OptionalThing<DfDecoMapPickup> optPickup = decoMapFile.readPickup(getProjectDir().getPath() + TEST_RESOURCES_PATH);
+        final DfDecoMapTablePart table = extractPickupTableAsOne(optPickup.get(), tableName);
+        final List<String> pieceCodeList = table.getPropertyList().stream().flatMap(property -> property.getPreviousPieceList().stream()).collect(Collectors.toList());
+        pieceCodeList.addAll(table.getPropertyList().stream().map(property -> property.getPieceCode()).collect(Collectors.toList()));
+        final DfDecoMapPiece piece = preparePiece(DfDecoMapPieceTargetType.Table, tableName, null, author, pieceCode, pieceCodeList);
+
+        // ## Act ##
+        DfDecoMapPickup mergedPickUp = decoMapFile.merge(optPickup, Collections.singletonList(piece));
+
+        // ## Assert ##
+        assertNotNull(mergedPickUp);
+        log(mergedPickUp);
+
+        DfDecoMapTablePart mergedTable = extractPickupTableAsOne(optPickup.get(), tableName);
+        assertNotNull(mergedTable);
+        assertEquals(tableName, mergedTable.getTableName());
+
+        List<DfDecoMapPropertyPart> mergedPropertyList = mergedTable.getPropertyList();
+        assertHasAnyElement(mergedPropertyList);
+
+        DfDecoMapPropertyPart mergedProperty = mergedPropertyList.get(0);
+        assertEquals(pieceCode, mergedProperty.getPieceCode());
+        assertEquals(author, mergedProperty.getPieceOwner());
+    }
+
+    public void test_merge_AllTestPiecesAndPickupAtResources() throws Exception {
+        // ## Arrange ##
+        final DfDecoMapFile decoMapFile = new DfDecoMapFile();
+        final List<DfDecoMapPiece> pieceList = decoMapFile.readPieceList(getProjectDir().getPath() + TEST_RESOURCES_PATH);
+        final OptionalThing<DfDecoMapPickup> optPickUp = decoMapFile.readPickup(getProjectDir().getPath() + TEST_RESOURCES_PATH);
+
+        // ## Act ##
+        final DfDecoMapPickup mergedPickUp = decoMapFile.merge(optPickUp, pieceList);
+
+        // ## Assert ##
+        assertNotNull(mergedPickUp);
+        log(mergedPickUp);
+        {
+            DfDecoMapTablePart member = extractPickupTableAsOne(mergedPickUp, "MEMBER");
+            assertEquals(1, member.getPropertyList().size()); // from piece
+            assertEquals("deco", member.getPropertyList().get(0).getPieceOwner());
+            assertEquals(3, member.getColumnList().size()); // from piece
+            {
+                DfDecoMapColumnPart birthdate = extractPickupColumnAsOne(member, "BIRTHDATE");
+                assertHasOnlyOneElement(birthdate.getPropertyList());
+                assertEquals("hakiba", birthdate.getPropertyList().get(0).getPieceOwner());
+            }
+        }
+        {
+            DfDecoMapTablePart login = extractPickupTableAsOne(mergedPickUp, "MEMBER_LOGIN");
+            assertEquals(1, login.getPropertyList().size()); // from pickup
+            assertEquals("cabos", login.getPropertyList().get(0).getPieceOwner());
+            assertEquals(2, login.getColumnList().size()); // from pickup
+            {
+                DfDecoMapColumnPart loginDatetime = extractPickupColumnAsOne(login, "LOGIN_DATETIME");
+                assertEquals(1, loginDatetime.getPropertyList().size()); // from pickup
+                assertEquals("hakiba", loginDatetime.getPropertyList().get(0).getPieceOwner());
+            }
+            {
+                DfDecoMapColumnPart statusCode = extractPickupColumnAsOne(login, "LOGIN_MEMBER_STATUS_CODE");
+                assertEquals(2, statusCode.getPropertyList().size()); // conflict from pickup
+                assertEquals("deco", statusCode.getPropertyList().get(0).getPieceOwner());
+            }
+        }
+        {
+            DfDecoMapTablePart purchase = extractPickupTableAsOne(mergedPickUp, "PURCHASE");
+            assertEquals(2, purchase.getPropertyList().size()); // conflict
+            assertEquals("deco", purchase.getPropertyList().get(0).getPieceOwner());
+            assertEquals(1, purchase.getColumnList().size()); // conflict
+            {
+                DfDecoMapColumnPart purchaseDatetime = extractPickupColumnAsOne(purchase, "PURCHASE_DATETIME");
+                assertEquals(3, purchaseDatetime.getPropertyList().size()); // conflict, should be cabos, hakiba, deco
+            }
+        }
+    }
+
+    // -----------------------------------------------------
+    //                                          Assist Logic
+    //                                          ------------
+    private DfDecoMapPiece preparePiece(DfDecoMapPieceTargetType type, String tableName, String columnName,
+                                        String author, String pieceCode, List<String> previousPieceList) {
         DfDecoMapPiece piece = new DfDecoMapPiece();
         piece.setFormatVersion("1.0");
         piece.setTableName(tableName);
         piece.setColumnName(columnName);
-        piece.setTargetType(DfDecoMapPieceTargetType.Column);
+        piece.setTargetType(type);
         piece.setDecomment("decomment");
-        piece.setDatabaseComment("databasecomment");
-        piece.setCommentVersion(commentVersion);
+        piece.setDatabaseComment("decomment does't mean database comment, means deco chan comment");
+        piece.setCommentVersion(1L);
         piece.addAllAuthors(Collections.singletonList(author));
-        piece.setPieceCode("DE000000");
-        piece.setPieceDatetime(decommentDateTime);
+        piece.setPieceCode(pieceCode);
+        piece.setPieceDatetime(currentLocalDateTime());
         piece.setPieceOwner(author);
-        piece.addAllPreviousPieces(Collections.emptyList());
+        piece.addAllPreviousPieces(previousPieceList);
         return piece;
+    }
+
+    private DfDecoMapTablePart extractPickupTableAsOne(DfDecoMapPickup pickup, String tableName) {
+        List<DfDecoMapTablePart> tableList = pickup.getTableList().stream().filter(table -> {
+            return table.getTableName().equals(tableName);
+        }).collect(Collectors.toList());
+        assertHasOnlyOneElement(tableList);
+        return tableList.get(0);
+    }
+
+    private DfDecoMapColumnPart extractPickupColumnAsOne(DfDecoMapTablePart table, String columnName) {
+        List<DfDecoMapColumnPart> columnList = table.getColumnList().stream()
+                .filter(column -> column.getColumnName().equals(columnName)).collect(Collectors.toList());
+        assertHasOnlyOneElement(columnList);
+        return columnList.get(0);
     }
 
     // ===================================================================================
