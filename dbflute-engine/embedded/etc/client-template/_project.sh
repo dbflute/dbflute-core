45e07b4ed1a5c484aa9e1b8a1784af77689f58b9
jflute <jflute@jflute-nyabara>
2019-12-05 15:10:07 +0900

    auto-setting for JAVA_HOME (keeping exiting JAVA_HOME)

--- dbflute-engine/embedded/etc/client-template/_project.sh
+++ dbflute-engine/embedded/etc/client-template/_project.sh
@@ -8,19 +8,9 @@ export MY_PROPERTIES_PATH=build.properties
 
 JAVA_VERSION=1.8+
 
-# Check JAVA_HOME exists
+# auto-setting for JAVA_HOME (keeping exiting JAVA_HOME)
 if [[ -z "${JAVA_HOME}" ]]; then
   if [[ `uname` = "Darwin" ]]; then
     export JAVA_HOME=$(/usr/libexec/java_home -v ${JAVA_VERSION})
-  else
-    echo "JAVA_HOME is not specified." >&2
-    exit 1
   fi
 fi
-
-# Check executable
-if [[ ! -x ${JAVA_HOME}/bin/java ]]; then
-  echo "Could not find Java executable in ${JAVA_HOME}" >&2
-  exit 1
-fi
-
