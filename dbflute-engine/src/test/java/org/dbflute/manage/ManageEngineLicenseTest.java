ac23d7eb7ef5fd98c25cc36f515bba8f36c49e35
jflute <jflute@jflute-nyabara>
2019-12-31 23:31:25 +0900

    Copyright 2014-2020 the original author or authors.

--- dbflute-engine/src/test/java/org/dbflute/manage/ManageEngineLicenseTest.java
+++ dbflute-engine/src/test/java/org/dbflute/manage/ManageEngineLicenseTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 the original author or authors.
+ * Copyright 2014-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ import org.dbflute.util.Srl;
  */
 public class ManageEngineLicenseTest extends EngineTestCase {
 
-    private static final String COPYRIGHT = "Copyright 2014-2019 the original author or authors.";
+    private static final String COPYRIGHT = "Copyright 2014-2020 the original author or authors.";
 
     public void test_licensed_main() {
         // ## Arrange ##
